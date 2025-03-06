50e315f20938aa71e9a5465548535925e640bc0f
Vasili Makarchuk <vasili.makarchuk@artezio.com>
2025-03-06 18:11:31 +0300

    commit

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ def create_data():
         db.add(asset)
         db.commit()
 
+        asset = Asset(name="ETH1")
+        db.add(asset)
+        db.commit()
+
         flow = Flow(amount=75, date="2023-10-01", asset=asset, institution=institution)
         db.add(flow)
         db.commit()
